70c2399181737b6d375778b65d4f8ca92226bf22
BitDragon256 <bergben44@gmail.com>
2023-12-01 11:36:42 +0100

    cleanup part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,7 +1,6 @@
 use std::fs;
 
  fn main() {
-    println!("Hello Day 1!");
     let input = fs::read_to_string("day01.txt").expect("Something went wrong reading the file");
     let splitted = input.split("\n").collect::<Vec<&str>>();
 
@@ -24,51 +23,57 @@ use std::fs;
         }
         first = None;
     }
-    println!("Part 1: {}", sum);
+    //println!("Part 1: {}", sum);
 
     // Part 2
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let rev_numbers = numbers.map(|n| n.chars().rev().collect::<String>());
+    let mut rev_str_numbers = ["";10];
+    (0..10).for_each(|i| rev_str_numbers[i] = rev_numbers[i].as_str() );
+
     let mut sum = 0;
     for word in &splitted {
-        let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-        let num = find_first_in(word, numbers);
-        let rev_numbers = numbers.map(|n| n.chars().rev().collect::<String>());
-        let mut rev_str_numbers = ["";10];
-        (0..10).for_each(|i| rev_str_numbers[i] = rev_numbers[i].as_str() );
-        let num2 = find_first_in(word.chars().rev().collect::<String>().as_str(), rev_str_numbers);
-        let conc = format!("{}{}", num, num2);
+        let first_str_num = find_first_in(word, numbers);
+        let last_num = find_first_in(word.chars().rev().collect::<String>().as_str(), rev_str_numbers);
+        let conc = format!("{}{}", first_str_num, last_num);
         sum += conc.parse::<i32>().unwrap();
+        // println!("{}", conc);
     }
-    println!("Part 2: {}", sum);
+    // println!("Part 2: {}", sum);
+    println!("{}", sum);
 }
 
-fn find_first_in(word: &str, numbers: [&str; 10]) -> usize {
-    let mut num = 100;
-    let mut first_real = 100;
+fn find_first_in(word: &str, numbers: [&str; 10]) -> u32 {
+    let mut num: Option<u32> = None;
+    let mut first_char_num: Option<u32> = None;
     let mut i = 0;
     for c in word.chars() {
         if c.is_digit(10) {
-            first_real = i;
+            first_char_num = Some(i);
             break;
         }
         i += 1;
     }
 
-    let mut first_num;
-    let mut last_first_num = 100;
+    let mut first_str_num: Option<u32>;
+    let mut best_first_str_num: Option<u32> = None;
     i = 0;
     for n in numbers {
-        first_num = match word.find(n) {
-            Some(v) => v,
-            None => 100
+        first_str_num = match word.find(n) {
+            Some(v) => Some(v as u32),
+            _ => None
         };
-        if first_num != 100 && last_first_num > first_num {
-            num = i;
-            last_first_num = first_num;
+        if best_first_str_num.is_none() || first_str_num.is_some() && first_str_num < best_first_str_num {
+            num = Some(i);
+            best_first_str_num = first_str_num;
         }
         i += 1;
     }
-    if last_first_num > first_real {
-        num = word.chars().nth(first_real).unwrap().to_digit(10).unwrap() as usize;
+    if best_first_str_num.is_none() || first_char_num.is_some() && first_char_num < best_first_str_num {
+        num = Some(word.chars().nth(first_char_num.unwrap() as usize).unwrap().to_digit(10).unwrap());
     }
-    num
+    match num {
+        Some(n) => n,
+        None => 0
+    }
 }
